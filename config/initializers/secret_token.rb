7b97aad0921d92ae64993964403f054c69772c93
Serj L <gelvior@gmail.com>
2014-05-13 00:58:01 +0400

    Project init with some gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Configio::Application.config.secret_key_base = '1ebb11610202c1759dc14167c46940aee7a6d99adab4d78e3fc70d28fa9c0096b3da638a89ce6f2f8f5f6622b2b159136a8d8a3243e9dcc9ef032b3f662e4fff'
